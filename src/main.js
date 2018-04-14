25b8d823a24041da4651b5ee5da1f763251f8ad9
José Javier Nieto de la Cuesta <josejaviernieto@gmail.com>
2018-04-15 01:23:19 +0200

    Added result from tutorial 3. Builder and extensions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
 module.exports.loop = function () {
   for(var name in Game.creeps) {
     var creep = Game.creeps[name];
@@ -9,6 +11,9 @@ module.exports.loop = function () {
     }
     if(creep.memory.role == 'upgrader') {
       roleUpgrader.run(creep);
+    }
+    if(creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
     }
   }
 }
