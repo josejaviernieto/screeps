a9e120973b3bfe11724fa98656dc91146ed6baff
José Javier Nieto de la Cuesta <josejaviernieto@gmail.com>
2018-04-15 01:30:11 +0200

    Added result form Tutorial 4: Auto-creeps and memory gestion

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+
+  for(var name in Memory.creeps) {
+    if(!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log('Clearing non-existing creep memory:', name);
+    }
+  }
+
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  console.log('Harvesters: ' + harvesters.length);
+  
+  if(harvesters.length < 2) {
+    var newName = 'Harvester' + Game.time;
+    console.log('Spawning new harvester: ' + newName);
+    Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+				     {memory: {role: 'harvester'}});        
+  }
+  
+  
   for(var name in Game.creeps) {
     var creep = Game.creeps[name];
 
