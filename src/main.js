81971618a0a664db7c8d4a17e324927d3bdbeb4b
José Javier Nieto de la Cuesta <josejaviernieto@gmail.com>
2018-04-15 03:19:01 +0200

    Changed the spawn system to spawn builders and updaters

--- src/main.js
+++ src/main.js
@@ -31,11 +31,27 @@ module.exports.loop = function () {
   if(harvesters.length < 2) {
     var newName = 'Harvester' + Game.time;
     console.log('Spawning new harvester: ' + newName);
-    Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+    Game.spawns['Homeworld'].spawnCreep([WORK,CARRY,MOVE], newName, 
 				     {memory: {role: 'harvester'}});        
   }
+
+  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
   
+  if(builders.length < 1) {
+    var newName = 'Builder' + Game.time;
+    console.log('Spawning new builder: ' + newName);
+    Game.spawns['Homeworld'].spawnCreep([WORK,CARRY,MOVE], newName, 
+				     {memory: {role: 'builder'}});        
+  }
+
+  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
   
+  if(harvesters.length < 2) {
+    var newName = 'Upgrader' + Game.time;
+    console.log('Spawning new upgrader: ' + newName);
+    Game.spawns['Homeworld'].spawnCreep([WORK,CARRY,MOVE], newName, 
+				     {memory: {role: 'upgrader'}});        
+  }
   for(var name in Game.creeps) {
     var creep = Game.creeps[name];
 
