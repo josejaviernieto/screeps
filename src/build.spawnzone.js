d49c8ed46b1c95b8abfc780c65f78963d780a52c
José Javier Nieto de la Cuesta <josejaviernieto@gmail.com>
2018-04-20 16:59:22 +0200

    correctind last commit

---
+++ src/build.spawnzone.js
@@ -0,0 +1,11 @@
+var buildSpawnZone = {
+
+  buildzone: function(pos){
+    
+    var obj=Game.rooms[pos.roomName].lookForAtArea(LOOK_STRUCTURES, pos.y-6, pos.x-6 , pos.y+6, pos.x+6, true);
+    
+    Game.rooms[pos.roomName].visual.rect(pos.x-6,pos.y-6, 13, 13);
+  }
+}
+
+module.exports = buildSpawnZone;
